414ca063a7055456e8d719195db04e8501a41fa9
anytodo92 <anything.todo292@gmail.com>
2024-02-15 10:34:22 +0100

    added wiki practice

--- gowiki/main.go
+++ gowiki/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -16,6 +19,8 @@ func (p *Page) save() error {
 	return os.WriteFile(fileName, p.Body, 0600)
 }
 
+var templates = template.Must(template.ParseFiles("view.html", "form.html"))
+
 func loadPage(title string) (*Page, error) {
 	body, error := os.ReadFile(title + ".txt")
 
@@ -26,13 +31,66 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func main() {
-	page1 := &Page{Title: "Test", Body: []byte("hello!")}
-	page1.save()
-	page2, error := loadPage("Test")
+func viewHandler(w http.ResponseWriter, req *http.Request) {
+	title := req.URL.Path[len("/view/"):]
+	page, error := loadPage(title)
+
 	if error != nil {
-		fmt.Println(string(page2.Body))
-	} else {
-		fmt.Printf("Encountered an error!!! %v", error.Error())
+		// page = &Page{Title: "Test"}
+		http.Redirect(w, req, fmt.Sprintf("/edit/%s", title), http.StatusFound)
+		return
 	}
+
+	renderTemplate("view", w, page)
+}
+
+func editHandler(w http.ResponseWriter, req *http.Request) {
+	title := req.URL.Path[len("/edit/"):]
+	page, error := loadPage(title)
+
+	if error != nil {
+		// page = &Page{Title: title}
+		http.Error(w, error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	renderTemplate("form", w, page)
+}
+
+func renderTemplate(fileName string, w http.ResponseWriter, page *Page) {
+	// t, error := template.ParseFiles(fmt.Sprintf("%s.html", fileName))
+
+	// if error != nil {
+	// 	http.Error(w, error.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// error = t.Execute(w, page)
+
+	error := templates.ExecuteTemplate(w, fmt.Sprintf("%s.html", fileName), page)
+
+	if error != nil {
+		http.Error(w, error.Error(), http.StatusInternalServerError)
+	}
+}
+
+func saveHandler(w http.ResponseWriter, req *http.Request) {
+	var title string = req.URL.Path[len("/save/"):]
+	var body string = req.FormValue("body")
+	var page *Page = &Page{Title: title, Body: []byte(body)}
+
+	error := page.save()
+	if error != nil {
+		http.Error(w, error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, req, fmt.Sprintf("/view/%v", title), http.StatusFound)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
